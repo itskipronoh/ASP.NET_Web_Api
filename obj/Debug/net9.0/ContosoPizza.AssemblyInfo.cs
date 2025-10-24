5ac6f02b0965833cf874a43fa81b6808655e8a14
itskipronoh <kipronoyegon68@gmail.com>
2025-10-24 14:31:58 +0300

    Update assembly information and cache files for ContosoPizza
    Signed-off-by: itskipronoh <kipronoyegon68@gmail.com>

--- obj/Debug/net9.0/ContosoPizza.AssemblyInfo.cs
+++ obj/Debug/net9.0/ContosoPizza.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ContosoPizza")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+938f85f6bc8d6be848d4d2f48e7d98f023032518")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ee2d7e1cda3b6484b2993c3a5f5cbda786b427e0")]
 [assembly: System.Reflection.AssemblyProductAttribute("ContosoPizza")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ContosoPizza")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
