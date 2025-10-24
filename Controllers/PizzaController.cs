de0a2739e356b0553e4c7534cad67426ba3e4b36
itskipronoh <kipronoyegon68@gmail.com>
2025-10-24 12:51:09 +0300

    Refactor readability of GET actions
    Signed-off-by: itskipronoh <kipronoyegon68@gmail.com>

--- Controllers/PizzaController.cs
+++ Controllers/PizzaController.cs
@@ -13,11 +13,21 @@ public class PizzaController : ControllerBase
     }
 
     // GET all action
-[HttpGet]
-public ActionResult<List<Pizza>> GetAll() =>
-    PizzaService.GetAll();
+    [HttpGet]
+    public ActionResult<List<Pizza>> GetAll() =>
+        PizzaService.GetAll();
 
     // GET by Id action
+    [HttpGet("{id}")]
+    public ActionResult<Pizza> Get(int id)
+    {
+        var pizza = PizzaService.Get(id);
+
+        if (pizza is null)
+            return NotFound();
+
+        return pizza;
+    }
 
     // POST action
 
