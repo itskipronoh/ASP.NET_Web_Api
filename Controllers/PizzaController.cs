938f85f6bc8d6be848d4d2f48e7d98f023032518
itskipronoh <kipronoyegon68@gmail.com>
2025-10-24 12:59:20 +0300

    Implement DELETE action in PizzaController
    Signed-off-by: itskipronoh <kipronoyegon68@gmail.com>

--- Controllers/PizzaController.cs
+++ Controllers/PizzaController.cs
@@ -55,4 +55,15 @@ public class PizzaController : ControllerBase
 
 
     // DELETE action
+    [HttpDelete("{id}")]
+    public ActionResult Delete(int id)
+    {
+        var pizza = PizzaService.Get(id);
+
+        if (pizza is null)
+            return NotFound();
+
+        PizzaService.Delete(id);
+        return NoContent();
+    }
 }
